fde9cd29b71ceaeeff4cb419c116af0a3bffee77
IceFairyFan <48240875+IceFairyFan@users.noreply.github.com>
2021-07-08 06:44:53 +0700

    Update ListController.java

--- src/main/java/com/application/posapplication/controller/ListController.java
+++ src/main/java/com/application/posapplication/controller/ListController.java
@@ -32,7 +32,7 @@ public class ListController {
         ArrayList<ListUserStoriesModel> lusmList = new ArrayList<>();
 
         try{
-            Connection conn = DriverManager.getConnection("jdbc:sqlserver://LAPTOP-J6HCJ4JQ\\SQLEXPRESS:1433;databaseName=DatabaseCapstone;user=sa;password=123456;");
+            Connection conn = DriverManager.getConnection("jdbc:sqlserver://databasecapstone.database.windows.net:1433;database=DatabaseCapstone;user=capstone@databasecapstone;password=P@ssw0rd;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
             String tableName = "dMeetingDetailsModalTable"; //"dMeetingDetailsTable";
             String columnName = "MeetingID";
             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM " + tableName + " WHERE " +columnName+ " = " + id);
